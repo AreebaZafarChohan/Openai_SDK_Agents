604a68a92f177b3f2d00d0899b6f6d23f3c47d9f
Areeba Zafar <165271432+AreebaZafarChohan@users.noreply.github.com>
2025-06-03 13:29:40 +0500

    adding 01 step and 05 step

---
+++ 01_uv/01_project/explore-uv/src/explore_uv/hello.py
@@ -0,0 +1,7 @@
+def main() -> None:
+    print("Hello from explore-uv from hello.py file!")
+    
+if __name__ == "__main__":
+    main()    
+    
+# to run this file run this comand in root ( uv run python src/explore_uv/hello.py )    
